2924b5e1bc4f08c119cf330caeb31fc5dab653fb
alkinuradia <112646930+alkinuradia@users.noreply.github.com>
2023-02-28 08:57:07 +0200

    first-and-second-hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,19 @@
-// import Player from '@vimeo/player';
-// import { throttle } from 'lodash';
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-// player.on('timeupdate',  throttle( elem => {
-//     localStorage.setItem('videoplayer-current-time', elem.seconds);
-//     }, 1000)
-//     );
+player.on('timeupdate',  throttle( elem => {
+    localStorage.setItem('videoplayer-current-time', elem.seconds);
+    }, 1000)
+    );
 
-// /*
-// player
-//     .setCurrentTime(localStorage.getItem('videoplayer-current-time')).catch(function (error) {
-//      console.error(error)
-//     // an error occurred setting the color
-// });
-// */
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+/*
+player
+    .setCurrentTime(localStorage.getItem('videoplayer-current-time')).catch(function (error) {
+     console.error(error)
+    // an error occurred setting the color
+});
+*/
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
